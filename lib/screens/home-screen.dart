bb2092449c5b7c871fb860083c067ab56d59d850
adhampton110 <anthony.hampton@slu.edu>
2023-02-14 18:26:28 -0600

    Organized File Structure
    - Created  and  directories.
    - Starter code from  has been moved into .
    - Removed  for it is an unnecessary file for our needs.

---
+++ lib/screens/home-screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Blank page, should be deleted once game-pin screen is uploaded
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
