2af9a3b94133d72f959257b5da97bdb0f53de656
Jake Wahle <37639498+jakewahle@users.noreply.github.com>
2023-02-28 18:50:29 -0600

    Changes to files after setting up firebase/firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'routes.dart';
 
 //proper "home" screen should be set when game-pin screen is uploaded
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       title: "SCRUM",
