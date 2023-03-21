e710bb45821a258eb5ef6b7597457cadbe321f25
Tony <tony.obradovic@slu.edu>
2023-03-21 13:47:29 -0500

    Successful read from realtime database to display top three users and their scores.

--- lib/screens/podium-screen.dart
+++ lib/screens/podium-screen.dart
@@ -42,28 +42,27 @@ class _PodiumScreenState extends State<PodiumScreen> {
   Map<String, dynamic> usersAndScores_sorted = {};
   List<MapEntry<String, dynamic>> sortedEntries = [];
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.white,
-    body: FutureBuilder<Map<String, dynamic>>(
-      future: getUsersAndScores('998765'), //hardcoded lobbyID. Will need to be changed where lobbyID is passed as parameter to constructor.
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          Map<String, dynamic> usersAndScores = snapshot.data!;
-          Map<String, dynamic> usersAndScores_sorted = sort(usersAndScores);
-          List<MapEntry<String, dynamic>> sortedEntries = usersAndScores_sorted.entries.toList();
-          return buildPodiumScreen(sortedEntries);
-        } else if (snapshot.hasError) {
-          return Center(child: Text("Error: ${snapshot.error}"));
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-  );
-}
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: getUsersAndScores('998765'), //hardcoded lobbyID. Will need to be changed where lobbyID is passed as parameter to constructor.
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Map<String, dynamic> usersAndScores = snapshot.data!;
+            Map<String, dynamic> usersAndScores_sorted = sort(usersAndScores);
+            List<MapEntry<String, dynamic>> sortedEntries = usersAndScores_sorted.entries.toList();
+            return buildPodiumScreen(sortedEntries);
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
 
   Widget buildPodiumScreen(List<MapEntry<String, dynamic>> sortedEntries) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -320,270 +319,4 @@ Widget build(BuildContext context) {
       ),
     );
   }
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    final scaffoldKey = GlobalKey<ScaffoldState>();
-
-    getUsersAndScores('998765').then((Map<String, dynamic> usersAndScores) {  //hardcoded lobbyID. Will need to be changed where lobbyID is passed as parameter to constructor.
-      usersAndScores_sorted = sort(usersAndScores);
-      sortedEntries = usersAndScores_sorted.entries.toList();
-      print(sortedEntries);     //populated
-      return Future.value(sortedEntries);
-    });
-    print(sortedEntries);       //not populated
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          //background image
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("../../assets/images/shapes-background.jpg"),
-                fit: BoxFit.cover,
-              )
-            )
-          ),
-          Container(
-            width: 1445.3,
-            height: 905.1,
-            decoration: BoxDecoration(
-              color: Color(0x00FFFFFF),
-            ),
-            alignment: AlignmentDirectional(0, 0),
-            child: Align(
-              alignment: AlignmentDirectional(0, 1),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  //Second Place Column
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
-                        child: AutoSizeText( "Second Name",
-                          //sortedEntries[1].value['nickname'],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Poppins',
-                            color: Colors.black,
-                            fontSize: 40,
-                          ),
-                        )
-                      ),
-                      Container(
-                        width: 350,
-                        height: 403.9,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF864CBF),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(0),
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(0),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              width: 120,
-                              height: 120,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("../../assets/images/silver-medal.png"),
-                                  fit: BoxFit.fitHeight,
-                                )
-                              )
-                            ),
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                              child: Text( "20",
-                                //sortedEntries[1].value['score'],
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  //First Place Column
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
-                        child: AutoSizeText( "First Name",
-                          //sortedEntries[0].value['nickname'],
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Poppins',
-                            color: Colors.black,
-                            fontSize: 40,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 350,
-                        height: 578.1,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF864CBF),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 20,
-                              color: Color(0x33000000),
-                              offset: Offset(0, 2),
-                            )
-                          ],
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(0),
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10),
-                          ),
-                        ),
-                       child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              width: 120,
-                              height: 120,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("../../assets/images/gold-medal.png"),
-                                  fit: BoxFit.fitHeight,
-                                )
-                              )
-                            ),
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                              child: Text( "30",
-                                //sortedEntries[0].value['score'],
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  //Third Place Column
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
-                        child: AutoSizeText( "Third Name",
-                          //sortedEntries[2].value['nickname'],
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Poppins',
-                            color: Colors.black,
-                            fontSize: 40,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 350,
-                        height: 301.4,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF864CBF),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 20,
-                              color: Color(0x33000000),
-                              offset: Offset(0, 2),
-                            )
-                          ],
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(0),
-                            topLeft: Radius.circular(0),
-                            topRight: Radius.circular(10),
-                          ),
-                        ),
-                       child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              width: 120,
-                              height: 120,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("../../assets/images/bronze-medal.png"),
-                                  fit: BoxFit.fitHeight,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                              child: Text( "10",
-                                //sortedEntries[2].value['score'],
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ]
-              ),
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(0.95, -0.95),
-            child: IconButton(
-              iconSize: 60,
-              color: Color(0xFF8E50EE),
-              icon: Icon(
-                Icons.home_rounded,
-                color: Color(0xFF8E50EE),
-                size: 75,
-              ),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    transitionDuration: Duration.zero,
-                    pageBuilder: (context, animation,
-                      secondaryAnimation) =>
-                        gamePinScreen(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }*/
 }
