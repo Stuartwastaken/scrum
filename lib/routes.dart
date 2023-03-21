0cf7162adf2d73cd2c2574bb43e2d0014e607338
Tony <tony.obradovic@slu.edu>
2023-03-21 13:01:39 -0500

    Push for Stuart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:scrum/screens/podium-screen.dart';
 import 'screens/forgot-password-screen.dart';
 import 'screens/game-pin-screen.dart';
 import 'screens/login-screen.dart';
 import 'screens/register-screen.dart';
+import 'screens/podium-screen.dart';
 
 var routes = <String, WidgetBuilder>{
   "/": (context) => const gamePinScreen(),
   "/login": (context) => const LoginScreen(),
   "/register": (context) => const Register(),
   "/forgot-password": (context) => const ForgotPasswordWidget(),
+  "/podium": (context) => const PodiumScreen(),
 };
