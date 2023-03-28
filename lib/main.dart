23b671e0bb647741e0c206258c4d6c0bdb70db3d
adhampton <anthony.hampton1235@gmail.com>
2023-03-28 15:39:35 -0500

    pushing changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:scrum/screens/game-pin-screen.dart';
+import 'package:scrum/screens/temp.dart';
 
 //proper "home" screen should be set when game-pin screen is uploaded
 void main() async {
@@ -15,7 +16,7 @@ void main() async {
   runApp(
     MaterialApp(
       title: "SCRUM",
-      home: GamePinScreen(),
+      home: Quiz(),
     ),
   );
 }
