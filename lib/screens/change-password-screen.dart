7905caf4ee42361e3beb970a88efc2e90b56afa4
Jake Wahle <37639498+jakewahle@users.noreply.github.com>
2023-03-27 19:18:39 -0500

    Added screen to change password

---
+++ lib/screens/change-password-screen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'edit-profile-screen.dart';
+
+class ChangePasswordScreen extends StatefulWidget {
+  const ChangePasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChangePasswordScreenState createState() => _ChangePasswordScreenState();
+}
+
+class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _oldPasswordController = TextEditingController();
+  final _newPasswordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  late User _currentUser;
+  late String _newPassword;
+
+  @override
+  void initState() {
+    _currentUser = FirebaseAuth.instance.currentUser!;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _oldPasswordController.dispose();
+    _newPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void _changePassword() async {
+    final form = _formKey.currentState;
+    if (form!.validate()) {
+      final oldPassword = _oldPasswordController.text;
+      final newPassword = _newPasswordController.text;
+      final confirmPassword = _confirmPasswordController.text;
+      if (newPassword != confirmPassword) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('New password and confirm password do not match'),
+          ),
+        );
+        return;
+      }
+      try {
+        final credential = EmailAuthProvider.credential(
+            email: _currentUser.email!, password: oldPassword);
+        await _currentUser.reauthenticateWithCredential(credential);
+        await _currentUser.updatePassword(newPassword);
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Password changed successfully'),
+          ),
+        );
+      } on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(e.message!),
+          ),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('An error occurred. Please try again.'),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Change Password'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Current Password',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your current password';
+                } else if (value.length < 6) {
+                  return 'Your password must be at least 6 characters long';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'New Password',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a new password';
+                } else if (value.length < 6) {
+                  return 'Your password must be at least 6 characters long';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Confirm New Password',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please confirm your new password';
+                } else if (value != _newPasswordController.text) {
+                  return 'Passwords do not match';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _changePassword,
+              child: Text('Change Password'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
