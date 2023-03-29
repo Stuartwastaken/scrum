3a7ac957c74542da98bb9334067359b7be53f5f6
Jake Wahle <37639498+jakewahle@users.noreply.github.com>
2023-03-28 19:01:44 -0500

    Fixed functionality

--- lib/screens/change-password-screen.dart
+++ lib/screens/change-password-screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'edit-profile-screen.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   final User user;
@@ -18,7 +17,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   final _confirmPasswordController = TextEditingController();
 
   late User _currentUser;
-  late String _newPassword;
 
   @override
   void initState() {
@@ -42,20 +40,23 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
       final confirmPassword = _confirmPasswordController.text;
       if (newPassword != confirmPassword) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
+          const SnackBar(
             content: Text('New password and confirm password do not match'),
           ),
         );
         return;
       }
+
       try {
         final credential = EmailAuthProvider.credential(
             email: _currentUser.email!, password: oldPassword);
         await _currentUser.reauthenticateWithCredential(credential);
         await _currentUser.updatePassword(newPassword);
-        Navigator.of(context).pop();
+        await _currentUser.reload();
+        final User? updatedUser = FirebaseAuth.instance.currentUser;
+        Navigator.pop(context, updatedUser);
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
+          const SnackBar(
             content: Text('Password changed successfully'),
           ),
         );
@@ -67,7 +68,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
         );
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
+          const SnackBar(
             content: Text('An error occurred. Please try again.'),
           ),
         );
@@ -79,63 +80,69 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Change Password'),
+        title: const Text('Change Password'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Current Password',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _oldPasswordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Current Password',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your current password';
+                  } else if (value.length < 6) {
+                    return 'Your password must be at least 6 characters long';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your current password';
-                } else if (value.length < 6) {
-                  return 'Your password must be at least 6 characters long';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'New Password',
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _newPasswordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'New Password',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a new password';
+                  } else if (value.length < 6) {
+                    return 'Your password must be at least 6 characters long';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter a new password';
-                } else if (value.length < 6) {
-                  return 'Your password must be at least 6 characters long';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Confirm New Password',
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _confirmPasswordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Confirm New Password',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please confirm your new password';
+                  } else if (value != _newPasswordController.text) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please confirm your new password';
-                } else if (value != _newPasswordController.text) {
-                  return 'Passwords do not match';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _changePassword,
-              child: Text('Change Password'),
-            ),
-          ],
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _changePassword,
+                child: const Text('Change Password'),
+              ),
+            ],
+          ),
         ),
       ),
     );
