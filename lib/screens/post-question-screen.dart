1028d90158b70c3bef0d9744c828d9b8ff06ae0c
Tony <tony.obradovic@slu.edu>
2023-04-06 11:39:26 -0500

    Updating Post-Question-Screen to have firebase access

--- lib/screens/post-question-screen.dart
+++ lib/screens/post-question-screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:tuple/tuple.dart';
+
 
 class PostQuestionScreenWidget extends StatefulWidget {
   final bool isCorrect;
-  const PostQuestionScreenWidget({Key? key, required this.isCorrect})
+  final String uid;
+  const PostQuestionScreenWidget({Key? key, required this.isCorrect, required this.uid})
       : super(key: key);
 
   @override
   _PostQuestionScreenWidgetState createState() =>
       _PostQuestionScreenWidgetState();
 }
+
+Future<Map<String, dynamic>> getUsersAndScores(String lobbyID) async {
+  final databaseRef = FirebaseDatabase.instance.ref();
+  Map<String, dynamic> usersInLobby = {};
+  await databaseRef.child(lobbyID).once().then((DatabaseEvent event) {
+    Map<dynamic, dynamic> lobbyData = event.snapshot.value as Map;
+    if (lobbyData != null) {
+      lobbyData.forEach((uid, userData) {
+        if (uid.toString().substring(0,3) == 'uid') {
+          String nickname = userData['nickname'];
+          int score = userData['score'];
+          usersInLobby[uid] = {
+            'nickname': nickname,
+            'score': score
+          };
+        };
+      });
+    }
+  });
+  return usersInLobby;
+}
+
+Map<String, dynamic> sort(Map<String, dynamic> usersAndScores){
+  List<MapEntry<String, dynamic>> usersList = usersAndScores.entries.toList();
+  usersList.sort((a,b) => b.value['score'].compareTo(a.value['score']));
+  usersAndScores = Map.fromEntries(usersList);
+  return usersAndScores;
+}
+
+Tuple2<String, String> getAbovePlayerInfo(List<MapEntry<String, dynamic>> sortedEntries, String uid) {
+  List<Map<String, dynamic>> listOfMaps = sortedEntries.map((entry) => {entry.key: entry.value}).toList();
+  print(listOfMaps);
+  int thisPlayer = listOfMaps.indexWhere((element) => element['nickname'] == uid[0]);
+
+  return Tuple2("one", "two");
+}
+
 
 class _PostQuestionScreenWidgetState extends State<PostQuestionScreenWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -25,8 +66,34 @@ class _PostQuestionScreenWidgetState extends State<PostQuestionScreenWidget> {
     super.dispose();
   }
 
-  @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: 
+        widget.isCorrect ? Color(0xFF66BF39) : Color(0xFFFF3355),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: getUsersAndScores('998765'),   //------ hardcoded lobbyID. Will need to be changed
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Map<String, dynamic> usersAndScores = snapshot.data!;
+            Map<String, dynamic> usersAndScores_sorted = sort(usersAndScores);
+            List<MapEntry<String, dynamic>> sortedEntries = usersAndScores_sorted.entries.toList();
+            Tuple2<String, String> abovePlayerInfo = getAbovePlayerInfo(sortedEntries, widget.uid);
+            return buildPostQuestionScreen(sortedEntries);
+          }
+          else if (snapshot.hasError) {
+            return Center(child: Text("Error:  ${snapshot.error}"));
+          }
+          else {
+            return Center(child: CircularProgressIndicator());
+          }
+        }
+      ),
+    );
+  }
+
+  @override
+  Widget buildPostQuestionScreen(List<MapEntry<String, dynamic>> sortedEntries) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       key: scaffoldKey,
       resizeToAvoidBottomInset: false,
