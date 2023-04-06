7c339c90cb5243ea47804e2552002a0031a31c98
Tony Obradovic <tobradovic@slu.edu>
2023-04-06 13:33:12 -0500

    adding firebase connectivity to screen. Need help finding source of error.

--- lib/screens/post-question-screen.dart
+++ lib/screens/post-question-screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:tuple/tuple.dart';
 
-
 class PostQuestionScreenWidget extends StatefulWidget {
   final bool isCorrect;
   final String uid;
-  const PostQuestionScreenWidget({Key? key, required this.isCorrect, required this.uid})
+  const PostQuestionScreenWidget(
+      {Key? key, required this.isCorrect, required this.uid})
       : super(key: key);
 
   @override
@@ -21,35 +21,51 @@ Future<Map<String, dynamic>> getUsersAndScores(String lobbyID) async {
     Map<dynamic, dynamic> lobbyData = event.snapshot.value as Map;
     if (lobbyData != null) {
       lobbyData.forEach((uid, userData) {
-        if (uid.toString().substring(0,3) == 'uid') {
+        if (uid.toString().substring(0, 3) == 'uid') {
           String nickname = userData['nickname'];
           int score = userData['score'];
-          usersInLobby[uid] = {
-            'nickname': nickname,
-            'score': score
-          };
-        };
+          usersInLobby[uid] = {'nickname': nickname, 'score': score};
+        }
+        ;
       });
     }
   });
   return usersInLobby;
 }
 
-Map<String, dynamic> sort(Map<String, dynamic> usersAndScores){
+Map<String, dynamic> sort(Map<String, dynamic> usersAndScores) {
   List<MapEntry<String, dynamic>> usersList = usersAndScores.entries.toList();
-  usersList.sort((a,b) => b.value['score'].compareTo(a.value['score']));
+  usersList.sort((a, b) => b.value['score'].compareTo(a.value['score']));
   usersAndScores = Map.fromEntries(usersList);
   return usersAndScores;
 }
 
-Tuple2<String, String> getAbovePlayerInfo(List<MapEntry<String, dynamic>> sortedEntries, String uid) {
-  List<Map<String, dynamic>> listOfMaps = sortedEntries.map((entry) => {entry.key: entry.value}).toList();
-  print(listOfMaps);
-  int thisPlayer = listOfMaps.indexWhere((element) => element['nickname'] == uid[0]);
-
-  return Tuple2("one", "two");
+Tuple3<String, String, String> getPlayerStatus(
+    List<MapEntry<String, dynamic>> sortedEntries, String uid) {
+  int index = -1;
+  for (int i = 0; i < sortedEntries.length; i++) {
+    MapEntry<String, dynamic> currentEntry = sortedEntries[i];
+    if (currentEntry.key == uid) {
+      index = i;
+      break;
+    }
+  }
+  //user uid not found. Fail gracefully.
+  if (index == -1) {
+    return Tuple3("-1", "Better luck ", "next time.");
+  }
+  //user is in first place
+  if (index == 0) {
+    return Tuple3("1", "You are in ", "1st place!");
+  } else {
+    int competitorScore = int.parse(sortedEntries[index - 1].value['score']);
+    int thisPlayerScore = int.parse(sortedEntries[index].value['score']);
+    String scoreDifference = (competitorScore - thisPlayerScore).toString();
+    String position = (index + 1).toString();
+    return Tuple3(
+        position, sortedEntries[index - 1].value['nickname'], scoreDifference);
+  }
 }
-
 
 class _PostQuestionScreenWidgetState extends State<PostQuestionScreenWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -68,37 +84,37 @@ class _PostQuestionScreenWidgetState extends State<PostQuestionScreenWidget> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: 
-        widget.isCorrect ? Color(0xFF66BF39) : Color(0xFFFF3355),
+      backgroundColor: widget.isCorrect ? Color(0xFF66BF39) : Color(0xFFFF3355),
       body: FutureBuilder<Map<String, dynamic>>(
-        future: getUsersAndScores('998765'),   //------ hardcoded lobbyID. Will need to be changed
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            Map<String, dynamic> usersAndScores = snapshot.data!;
-            Map<String, dynamic> usersAndScores_sorted = sort(usersAndScores);
-            List<MapEntry<String, dynamic>> sortedEntries = usersAndScores_sorted.entries.toList();
-            Tuple2<String, String> abovePlayerInfo = getAbovePlayerInfo(sortedEntries, widget.uid);
-            return buildPostQuestionScreen(sortedEntries);
-          }
-          else if (snapshot.hasError) {
-            return Center(child: Text("Error:  ${snapshot.error}"));
-          }
-          else {
-            return Center(child: CircularProgressIndicator());
-          }
-        }
-      ),
+          future: getUsersAndScores(
+              '998765'), //------ hardcoded lobbyID. Will need to be changed
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Map<String, dynamic> usersAndScores = snapshot.data!;
+              Map<String, dynamic> usersAndScores_sorted = sort(usersAndScores);
+              List<MapEntry<String, dynamic>> sortedEntries =
+                  usersAndScores_sorted.entries.toList();
+              Tuple3<String, String, String> playerStatus =
+                  getPlayerStatus(sortedEntries, widget.uid);
+              return buildPostQuestionScreen(
+                  playerStatus.item1, playerStatus.item2, playerStatus.item3);
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Error:  ${snapshot.error}"));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 
   @override
-  Widget buildPostQuestionScreen(List<MapEntry<String, dynamic>> sortedEntries) {
+  Widget buildPostQuestionScreen(
+      String position, String abovePlayer, String scoreDifference) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       key: scaffoldKey,
       resizeToAvoidBottomInset: false,
-      backgroundColor:
-          widget.isCorrect ? Color(0xFF66BF39) : Color(0xFFFF3355),
+      backgroundColor: widget.isCorrect ? Color(0xFF66BF39) : Color(0xFFFF3355),
       body: SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
@@ -160,7 +176,7 @@ class _PostQuestionScreenWidgetState extends State<PostQuestionScreenWidget> {
                     Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
                       child: Text(
-                        'You\'re in 2nd place\n10 points behind Jake',
+                        'Position: $position. You are $scoreDifference points behind $abovePlayer!',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontFamily: 'Poppins',
