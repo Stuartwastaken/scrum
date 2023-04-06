25351ed3f7ba56d3108ebca06e3a5f15093772b4
Tony Obradovic <tobradovic@slu.edu>
2023-04-06 20:42:02 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:scrum/screens/game-pin-screen.dart';
-import 'package:scrum/screens/post-question-screen.dart';
-import 'package:scrum/screens/podium-screen.dart';
 
 //proper "home" screen should be set when game-pin screen is uploaded
 void main() async {
@@ -17,8 +15,7 @@ void main() async {
   runApp(
     MaterialApp(
       title: "SCRUM",
-      home: PostQuestionScreenWidget(
-          uid: "uid2", isCorrect: true, pointsGained: 456),
+      home: GamePinScreen();
     ),
   );
 }
