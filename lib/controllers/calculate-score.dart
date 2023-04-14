cefd1247d9cf02376c498cb7bbc98c4bf73f941a
AndrewC <chena9654@parkwayschools.com>
2023-04-14 11:43:10 -0500

    post question screen now shows added points based on the time value

---
+++ lib/controllers/calculate-score.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'quiz-time-stream.dart';
+import 'package:scrum/utils/fire_RTdatabase.dart';
+
+class CalculateScore {
+  static Future<int> calculateAddValue(String quizID) async {
+    // Get the remaining time from the database
+    int? remainingTime = await ScrumRTdatabase.getTime(quizID);
+
+    // Do your calculations using remainingTime as an int value
+    int score = remainingTime! * 30;
+
+    print('Score: $score');
+    return score;
+  }
+}
