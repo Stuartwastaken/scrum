b92b377551b95a9acac97f2659462d65e2633d53
adhampton <anthony.hampton1235@gmail.com>
2023-04-21 07:38:28 -0500

    UI Refresh + Removed Page Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:scrum/screens/login-screen.dart';
 import 'firebase_options.dart';
 import 'routes.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,8 +16,7 @@ void main() async {
   runApp(
     MaterialApp(
       title: "SCRUM",
-      home: GamePinScreen(),
+      home: LoginScreen(),
     ),
   );
 }
-
