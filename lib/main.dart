39e24f4c74509edf8d07c50534b1a541a40e3387
Tony Obradovic <tobradovic@slu.edu>
2023-04-23 21:05:29 +0000

    Update main.dart Removed unused imports. Changed home screen back to GamePinScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:scrum/screens/host-lobby-screen.dart';
 import 'firebase_options.dart';
-import 'routes.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:scrum/screens/host-lobby-screen.dart';
+import 'package:scrum/screens/game-pin-screen.dart';
 
 //proper "home" screen should be set when game-pin screen is uploaded
 void main() async {
@@ -16,7 +12,7 @@ void main() async {
   runApp(
     MaterialApp(
       title: "SCRUM",
-      home: HostLobbyScreen(document: '998765'),
+      home: GamePinScreen(),
     ),
   );
 }
