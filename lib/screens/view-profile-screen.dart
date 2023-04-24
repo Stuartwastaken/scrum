7fa3d82f0e5be322790a6aea98f4f25c95a06345
AndrewC <chena9654@parkwayschools.com>
2023-04-24 16:05:35 -0500

    view profile ui revamped

--- lib/screens/view-profile-screen.dart
+++ lib/screens/view-profile-screen.dart
@@ -28,7 +28,19 @@ class _ViewProfileScreenState extends State<ViewProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: TextButton(
+          child: Text(
+            'SCRUM',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: Colors.deepOrange,
+            ),
+          ),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.deepOrange,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () async {
@@ -37,41 +49,111 @@ class _ViewProfileScreenState extends State<ViewProfileScreen> {
           },
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Username: $_username',
-              style: TextStyle(fontSize: 20),
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.0, 0.35, 1.0], // set stops for the gradient
+              colors: [
+                Color.fromARGB(255, 161, 15, 223),
+                Color.fromARGB(255, 251, 153, 42),
+                Color.fromARGB(255, 63, 3, 192), // add a third color
+              ],
             ),
-            SizedBox(height: 10),
-            Text(
-              'Email: $_email',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(
-                  MaterialPageRoute(
-                    builder: (context) => EditProfileScreen(user: _currentUser),
-                  ),
-                )
-                    .then((updatedUser) {
-                  setState(() {
-                    _currentUser = updatedUser;
-                    _username = _currentUser.displayName;
-                    _email = _currentUser.email;
-                  });
-                });
-              },
-              child: Text('Edit Profile'),
-            ),
-          ],
-        ),
-      ),
+          ),
+          child: Stack(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Profile Information",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 40,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 4),
+                    Container(
+                      width: 400,
+                      height: 175,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors
+                              .white, // Set the color of the outline to white
+                          width: 2, // Set the width of the outline to 2 pixels
+                        ),
+                        borderRadius: BorderRadius.circular(
+                            10), // Set the border radius to 10 pixels
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Username: $_username',
+                            style: TextStyle(
+                              color:
+                                  Colors.white, // Set the text color to white
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Email: $_email',
+                            style: TextStyle(
+                              color:
+                                  Colors.white, // Set the text color to white
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                EditProfileScreen(user: _currentUser),
+                          ),
+                        )
+                            .then((updatedUser) {
+                          setState(() {
+                            _currentUser = updatedUser;
+                            _username = _currentUser.displayName;
+                            _email = _currentUser.email;
+                          });
+                        });
+                      },
+                      child: Text(
+                        "Edit Profile",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                        side: BorderSide(
+                            width: 2.0,
+                            color: Color.fromARGB(
+                                255, 255, 255, 255)), // foreground color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              7), // set the desired border radius here
+                        ),
+                        minimumSize: Size(100, 40),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
