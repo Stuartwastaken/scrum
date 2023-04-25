7e90dbe8b1c1ada294c5889270aa8bd722f1c4c8
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-04-25 18:53:21 -0500

    GitHub Pages v1.0.0

--- android/app/src/main/kotlin/com/example/scrum/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scrum
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controllers/calculate-score.dart
+++
@@ -1,6 +0,0 @@
-class CalculateScore {
-  static int calculateAddValue(int remainingTime) {
-    int score = remainingTime * 20 + 100;
-    return score;
-  }
-}
